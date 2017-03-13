f9e1e568edc6db3d7c01c83d0f238db320154823
elvtn <mike@elvtn.com>
2017-03-12 21:59:51 -0600

    Create zillow-wordpress-plugin.php

---
+++ zillow-wordpress-plugin.php
@@ -0,0 +1,11 @@
+<?php
+   /*
+   Plugin Name: Zillow WordPress Plugin
+   Plugin URI: https://github.com/elvtn/zillow-wordpress-plugin
+   Description: Plugin to display Zillow data using widgets.
+   Version: 1.0
+   Author: Elvtn, LLC
+   Author URI: https://elvtn.com
+   License: GPL2
+   */
+?>
