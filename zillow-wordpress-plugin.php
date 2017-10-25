7884cab5cc19614941b6731cfbbd4dd57e095d8d
Michael Pratt <mpratt@homeadvisor.com>
2017-10-24 20:45:03 -0600

    Update to 1.3

--- zillow-wordpress-plugin.php
+++ zillow-wordpress-plugin.php
@@ -3,7 +3,7 @@
    Plugin Name: Zillow WordPress Plugin
    Plugin URI: https://github.com/elvtn/zillow-wordpress-plugin
    Description: Plugin to display Zillow data using widgets.
-   Version: 1.2
+   Version: 1.3
    Author: Elvtn, LLC
    Author URI: https://elvtn.com
    License: GPL2
